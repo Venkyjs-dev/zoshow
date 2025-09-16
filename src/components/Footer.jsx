82d7eb4b8c18fea481421e17990ec5b48aa7e0cc
Devi <devi131780@gmail.com>
2025-09-16 17:36:41 +0530

    refactor: Updating content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4 mt-auto">
-      &copy; {new Date().getFullYear()} MyBusiness. All rights reserved.
+      &copy; {new Date().getFullYear()} ZoShow. All rights reserved.
     </footer>
   );
 }
